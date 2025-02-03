3ccf3f64751640c78b7c28b299d50d922a729cdf
Ayush0115D <dhakreayush578@gmail.com>
2025-02-03 20:07:37 +0530

    Add files via upload

---
+++ domp2.js
@@ -0,0 +1,28 @@
+// let div =document.querySelector("div");
+// console.log(div);
+// let id=div.getAttribute("id");
+// console.log(id);
+// let name=div.getAttribute("name");
+// // console.log(name);
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class","newclass")); // elements me change hojata..console me show na hota
+// // let div =document.querySelector("div");
+// div.style.backgroundColor="green";
+// div.style.fontSize="25px";
+// div.innerText="helloo";
+// div.style.visibility="hidden";
+let newBtn=document.createElement("button");
+newBtn.innerText="click me!";
+console.log(newBtn);
+// let div=document.querySelector("div");
+// div.append(newBtn);
+// div.before(newBtn);
+ let p=document.querySelector("p");
+p.after(newBtn);
+
+let newHeading =document.createElement("h1");
+newHeading.innerHTML=" <i> hi, i am new</i>";
+document.querySelector("body").prepend(newHeading);
+let para=document.querySelector("p");
+para.remove();
+
