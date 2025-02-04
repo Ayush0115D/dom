eb35b1c815267e1025a002c2f6cab6508d0dec67
Ayush0115D <dhakreayush578@gmail.com>
2025-02-04 16:40:43 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,11 @@
+let heading= document.getElementById("myid"); //h1
+console.dir(heading);
+let headings= document.getElementsByClassName("heading-class");
+console.dir(headings);
+console.log(headings);
+let paragraphs=document.getElementsByTagName("p");
+console.dir(paragraphs);
+let firstel=document.querySelector("p"); //1st element
+console.dir(firstel);
+let all=document.querySelectorAll("p"); // all elements
+console.dir(all); 
